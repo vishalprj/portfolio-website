4408a41b91254ced509edf599b64d64d2c2dbb65
avipj <vishalpraj188@gmail.com>
2021-09-09 23:59:50 +0530

    updated modal

--- js/main.js
+++ js/main.js
@@ -1,28 +1,50 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const body = document.querySelector('body');
-const header = document.querySelector('.navbar');
-const overlay = document.querySelector('.overlay');
-const fadeElems = document.querySelectorAll('.has-fade');
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".navbar");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
-btnHamburger.addEventListener('click', function(){
-  console.log('click hamburger');
+btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
 
-  if(header.classList.contains('open')){ // Close Hamburger Menu
-    body.classList.remove('noscroll');
-    header.classList.remove('open');    
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
+  if (header.classList.contains("open")) {
+    // Close Hamburger Menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    closeModal();
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
     });
-    
+  } else {
+    // Open Hamburger Menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    openModal();
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
-  else { // Open Hamburger Menu
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
-
-  }  
 });
+
+//open close modal;
+function closeModal() {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  header.classList.remove("open");
+}
+function openModal() {
+  modal.style.display = "block";
+  overlay.style.display = "block";
+  header.classList.add("open");
+}
+
+window.addEventListener("click", outsideClick);
+
+const modal = document.querySelector(".modal");
+function outsideClick(e) {
+  if (e.target == modal) {
+    closeModal();
+  }
+}
